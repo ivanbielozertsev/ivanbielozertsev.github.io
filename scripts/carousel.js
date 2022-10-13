c51d73fa6edd519379554b5a83869ff0ab102544
ivanbielozertsev <90478135+ivanbielozertsev@users.noreply.github.com>
2022-10-13 15:58:38 +0200

    Test for Glivera

--- scripts/carousel.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    autoplay: true,
-    loop: true,
-    responsiveRefreshRate: 50,
-  });
-});
--- scripts/mobile-menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $("#mobile-menu-button").click(function() {
-    $("#mobile-nav").css("display", "block");
-  });
-
-  $("#mobile-nav-close").click(function() {
-    $("#mobile-nav").css("display", "none");
-  });
-});
